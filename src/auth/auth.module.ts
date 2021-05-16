d107f5aeecaa4d8cc09ac41b2dc3ba5f8546a99a
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-05-17 01:08:42 +0600

    remove unwanted code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
-import { GoogleStrategy } from '../google.strategy';
-
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
+  providers: [AuthService],
 })
 export class AuthModule {}
