7a92f195557098d8e1337266c5084c31c4837fa6
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-06-27 00:04:57 +0600

    return jwt token after user creation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './strategy/google-strategy';
 import { UserService } from '../users/users.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, GoogleStrategy, UserService],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { BadRequestException, ForbiddenException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 import { UserService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async googleLogin(req) {
     if (!req.user) throw new BadRequestException();
-    return await this.userService.createUser(req.user);
+
+    //save the user in the database
+    const savedUser = await this.userService.createUser(req.user);
+    console.log('Here is the saved user', savedUser);
+    //return the jwt token
+    const payload = { username: savedUser.firstName, sub: savedUser.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
