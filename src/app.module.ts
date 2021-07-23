0058324dd0a6d91653453a73079b57343a19f159
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-07-23 14:11:06 +0600

    remove google module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { YoutubeModule } from './youtube/youtube.module';
-import { GoogleModule } from './google/google.module';
 
 import { configService } from './config/config.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 // const modules = [YoutubeModule, GoogleModule, AuthModule, UsersModule];
-const modules = [YoutubeModule, GoogleModule];
+const modules = [YoutubeModule];
 
 @Module({
   imports: [...modules, GraphQLModule.forRoot(configService.getGqlConfig())],
--- src/google/google.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GoogleResolver } from './google.resolver';
-
-@Module({
-  providers: [GoogleResolver],
-})
-export class GoogleModule {}
