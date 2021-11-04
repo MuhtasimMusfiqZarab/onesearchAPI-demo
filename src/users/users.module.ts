ce2512b852978322b7c4e22f7bcdc8af236afdfa
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-11-04 22:42:15 +0600

    server path change in env file

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './users.service';
 import UserResolver from './users.resolver';
-import { ConfigModule } from '@nestjs/config';
 import { UserRepository } from './user.repository';
 import User from './user.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     TypeOrmModule.forFeature([UserRepository]),
     TypeOrmModule.forRoot({
       type: 'mysql',
