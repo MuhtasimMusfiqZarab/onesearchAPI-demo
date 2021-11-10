ae7db22338e9b0da01c175e347569bd81a6370f6
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-11-10 13:45:16 +0600

    connecting database from only app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { YoutubeModule } from './youtube/youtube.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { StripeModule } from './stripe/stripe.module';
 import { join } from 'path';
+
+import User from './users/user.entity';
+import { Youtube } from './youtube/youtube.entity';
 
 import { ConfigModule } from '@nestjs/config';
 
@@ -21,6 +25,16 @@ const modules = [YoutubeModule, AuthModule, UsersModule, StripeModule];
       // sorting the schema lexicographically
       sortSchema: true,
       playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.USER_DB,
+      entities: [User, Youtube],
+      synchronize: true,
     }),
   ],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,24 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './users.service';
 import UserResolver from './users.resolver';
 import { UserRepository } from './user.repository';
-import User from './user.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserRepository]),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.USER_DB,
-      entities: [User],
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [UserService, UserResolver],
-
   //this service can be used by other modules (ex Auth Module)
   exports: [UserService],
 })
