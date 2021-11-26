fd4abf2b260c38f81668012c307211d454cf9a65
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-11-26 17:42:49 +0600

    structure app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,36 @@
+//npm modules
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { join } from 'path';
+
+//custom modules
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { YoutubeModule } from './youtube/youtube.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { StripeModule } from './stripe/stripe.module';
-import { join } from 'path';
 
+//entities
 import User from './users/user.entity';
 import { Youtube } from './youtube/youtube.entity';
 import { Subscription } from './subscriptions/subscription.entity';
 
-import { ConfigModule } from '@nestjs/config';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
-
-const modules = [YoutubeModule, AuthModule, UsersModule, StripeModule];
+const modules = [
+  YoutubeModule,
+  AuthModule,
+  UsersModule,
+  StripeModule,
+  SubscriptionsModule,
+];
 
 @Module({
   imports: [
     ...modules,
     ConfigModule.forRoot(),
     GraphQLModule.forRoot({
-      // defining the generated schema file name and location
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
       playground: false,
@@ -36,7 +45,6 @@ const modules = [YoutubeModule, AuthModule, UsersModule, StripeModule];
       entities: [User, Youtube, Subscription],
       synchronize: true,
     }),
-    SubscriptionsModule,
   ],
 })
 export class AppModule {}
