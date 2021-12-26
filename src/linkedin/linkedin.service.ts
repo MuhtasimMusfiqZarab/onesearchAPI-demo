17aa8350a8f1ea37cd54de705435465b0f27f5e7
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-12-26 13:21:34 +0600

    resolve error issue

--- src/linkedin/linkedin.service.ts
+++ src/linkedin/linkedin.service.ts
@@ -1,13 +1,6 @@
 import { Injectable } from '@nestjs/common';
-
 import { LinkedinRepository } from './linkedin.repository';
-import { LinkedinType } from './linkedin.type';
-
 import { InjectRepository } from '@nestjs/typeorm';
-
-import { getConnection } from 'typeorm';
-
-import Linkedin from './linkedin.entity';
 
 @Injectable()
 export class LinkedinService {
@@ -18,15 +11,7 @@ export class LinkedinService {
 
   async addLinkedinLeads(input: any): Promise<any> {
     try {
-      const response = await getConnection()
-        .createQueryBuilder()
-        .insert()
-        .into(Linkedin)
-        .values(input)
-        .execute();
-
-      console.log('This is the resposne', response);
-      return response;
+      return await this.linkedinRepository.save(input);
     } catch (error) {
       throw new Error(error);
     }
