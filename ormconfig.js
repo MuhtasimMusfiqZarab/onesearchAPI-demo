6024d284d37ee0e38017e962cb4ca714132eb4a8
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-12-28 14:23:17 +0600

    environment specific database connection setup

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,35 @@
-module.exports = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ['**/*.entity.js'],
-  synchronize: true,
+var dbConfig = {
+  synchronize: false,
 };
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['**/*.entity.js'],
+    });
+    break;
+
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['**/*.entity.js'],
+    });
+    break;
+
+  default:
+    throw new Error('Unknown environment');
+    break;
+}
+
+module.exports = dbConfig;
