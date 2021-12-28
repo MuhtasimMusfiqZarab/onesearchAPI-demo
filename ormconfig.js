b778be9950c87543fa2a43e1e3bb8130e2b2fcad
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-12-28 14:16:00 +0600

    get config data from ormconfig for nest js

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,10 @@
 module.exports = {
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 3306,
-  username: 'test',
-  password: 'test',
-  database: 'test',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['**/*.entity.js'],
+  synchronize: true,
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -35,16 +35,8 @@ const modules = [
       sortSchema: true,
       playground: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['**/*.entity.js'],
-      synchronize: true,
-    }),
+    //get configuration data from ormconfig.js
+    TypeOrmModule.forRoot(),
   ],
 })
 export class AppModule {}
