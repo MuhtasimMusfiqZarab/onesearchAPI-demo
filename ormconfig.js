86af5d0bcd8778a68f8fad1a877cc55f17e16839
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2022-01-02 16:50:33 +0600

    remove migration file

--- ormconfig.js
+++ ormconfig.js
@@ -1,49 +1,57 @@
 var dbConfig = {
-  migrations: ['migration/*.js'],
-  cli: {
-    migrationsDir: 'migration',
-  },
+  // migrations: ['migration/*.js'],
+  // cli: {
+  //   migrationsDir: 'migration',
+  // },
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['**/*.entity.js'],
+  synchronize: true,
 };
 
-switch (process.env.NODE_ENV) {
-  case 'development':
-    Object.assign(dbConfig, {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['**/*.entity.js'],
-      synchronize: true,
-    });
-    break;
+// switch (process.env.NODE_ENV) {
+//   case 'development':
+//     Object.assign(dbConfig, {
+//       type: 'mysql',
+//       host: process.env.DB_HOST,
+//       port: 3306,
+//       username: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       entities: ['**/*.entity.js'],
+//       synchronize: true,
+//     });
+//     break;
 
-  case 'production':
-    Object.assign(dbConfig, {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['**/*.entity.js'],
-      synchronize: true,
-    });
-    break;
+//   case 'production':
+//     Object.assign(dbConfig, {
+//       type: 'mysql',
+//       host: process.env.DB_HOST,
+//       port: 3306,
+//       username: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       entities: ['**/*.entity.js'],
+//       synchronize: true,
+//     });
+//     break;
 
-  default:
-    Object.assign(dbConfig, {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['**/*.entity.js'],
-      synchronize: true,
-    });
-    break;
-}
+//   default:
+//     Object.assign(dbConfig, {
+//       type: 'mysql',
+//       host: process.env.DB_HOST,
+//       port: 3306,
+//       username: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       entities: ['**/*.entity.js'],
+//       synchronize: true,
+//     });
+//     break;
+// }
 
 module.exports = dbConfig;
