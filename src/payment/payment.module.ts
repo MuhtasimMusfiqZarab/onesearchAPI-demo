7d0d129d6b2a4b7ea3083864bd55951a299fb333
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2022-01-03 18:10:42 +0600

    add payment repository

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PaymentResolver } from './payment.resolver';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { PaymentRepository } from './payment.repository';
+
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../user/user.module';
 
 @Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forFeature([PaymentRepository]),
+  ],
   providers: [PaymentResolver, PaymentService],
-  controllers: [PaymentController]
+  controllers: [PaymentController],
 })
 export class PaymentModule {}
