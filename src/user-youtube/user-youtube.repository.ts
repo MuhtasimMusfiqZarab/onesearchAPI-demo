2e64f8ed5bec4706e54458973bd2446125ab4336
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2023-03-15 17:59:01 +0600

    unlock linkedin table fix

--- src/user-youtube/user-youtube.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Repository, EntityRepository } from 'typeorm';
-import UserYoutube from './user-youtube.entity';
-
-@EntityRepository(UserYoutube)
-export class UserYoutubeRepository extends Repository<UserYoutube> {}
